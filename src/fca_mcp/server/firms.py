86c2983cdbffb9297585a5248b6347d6743bcd00
woo <adresshfx@gmail.com>
2026-02-22 16:21:27 +0500

    add tools: get_firm_names, get_firm_addresses, get_firm_appointed_representatives

--- src/fca_mcp/server/firms.py
+++ src/fca_mcp/server/firms.py
@@ -33,5 +33,32 @@ async def get_firm(frn: str, fca_client: fca_api.async_api.Client = deps.FcaApiD
     return result.model_dump(mode="json")
 
 
+@firms_mcp.tool
+async def get_firm_names(frn: str, fca_client: fca_api.async_api.Client = deps.FcaApiDep) -> fca_api.types.pagination.MultipageList[fca_api.types.firm.FirmNameAlias]:
+    """Get firm names by FRN"""
+    out = await fca_client.get_firm_names(frn)
+    els = out.local_items()
+    out = types.PaginatedList[fca_api.types.firm.FirmNameAlias](items=els)
+    return out.model_dump(mode="json")
+
+
+@firms_mcp.tool
+async def get_firm_adresses(frn: str, fca_client: fca_api.async_api.Client = deps.FcaApiDep) -> fca_api.types.pagination.MultipageList[fca_api.types.firm.FirmAddress]:
+    """Get firm addresses by FRN"""
+    out = await fca_client.get_firm_addresses(frn)
+    els = out.local_items()
+    out = types.PaginatedList[fca_api.types.firm.FirmAddress](items=els)
+    return out.model_dump(mode="json")
+
+
+@firms_mcp.tool
+async def get_firm_appointed_representatives(frn: str, fca_client: fca_api.async_api.Client = deps.FcaApiDep) -> fca_api.types.pagination.MultipageList[fca_api.types.firm.FirmAppointedRepresentative]:
+    """Get firm appointed representatives by FRN"""
+    out = await fca_client.get_firm_appointed_representatives(frn)
+    els = out.local_items()
+    out = types.PaginatedList[fca_api.types.firm.FirmAppointedRepresentative](items=els)
+    return out.model_dump(mode="json")
+
+
 def get_server() -> fastmcp.FastMCP:
     return firms_mcp
