b920bd8842fc40f6d371336bdb3e9fc0a58f72b8
Ilja Orlovs <VRGhost@users.noreply.github.com>
2026-03-01 22:09:30 +0000

    x

--- src/fca_mcp/server/funds.py
+++ src/fca_mcp/server/funds.py
@@ -10,9 +10,7 @@ funds_mcp = fastmcp.FastMCP("search-funds", on_duplicate="error")
 
 
 @funds_mcp.tool
-async def get_fund(
-    prn: str, fca_client: fca_api.async_api.Client = deps.FcaApiDep
-) -> types.products.ProductDetails:
+async def get_fund(prn: str, fca_client: fca_api.async_api.Client = deps.FcaApiDep) -> types.products.ProductDetails:
     """Get fund details by PRN"""
     out = await fca_client.get_fund(prn)
     return types.products.ProductDetails.from_api_t(out)
@@ -21,22 +19,26 @@ async def get_fund(
 @funds_mcp.tool
 async def get_fund_names(
     prn: str, fca_client: fca_api.async_api.Client = deps.FcaApiDep
-) -> fca_api.types.pagination.MultipageList[fca_api.types.products.ProductNameAlias]:
+) -> types.list_t.PaginatedList[types.products.ProductNameAlias]:
     """Get fund names by PRN"""
     out = await fca_client.get_fund_names(prn)
     els = out.local_items()
-    out = types.list_t.PaginatedList[fca_api.types.products.ProductNameAlias](items=els)
+    out = types.list_t.PaginatedList[types.products.ProductNameAlias](
+        items=[types.products.ProductNameAlias.from_api_t(el) for el in els], start_index=0, has_next=False
+    )
     return out
 
 
 @funds_mcp.tool
 async def get_fund_subfunds(
     prn: str, fca_client: fca_api.async_api.Client = deps.FcaApiDep
-) -> fca_api.types.pagination.MultipageList[fca_api.types.products.SubFundDetails]:
+) -> types.list_t.PaginatedList[types.products.SubFundDetails]:
     """Get fund sub-funds by PRN"""
     out = await fca_client.get_fund_subfunds(prn)
     els = out.local_items()
-    out = types.list_t.PaginatedList[fca_api.types.products.SubFundDetails](items=els)
+    out = types.list_t.PaginatedList[types.products.SubFundDetails](
+        items=[types.products.SubFundDetails.from_api_t(el) for el in els], start_index=0, has_next=False
+    )
     return out
 
 
--- tests/server/test_funds_simple.py
+++ tests/server/test_funds_simple.py
@@ -12,4 +12,28 @@ async def test_get_fund(mcp_client: Client[FastMCPTransport]):
         },
     )
     assert tool_result is not None
-    assert 'SI000001' in str(tool_result.data)
+    assert "SI000001" in str(tool_result.data)
+
+
+@pytest.mark.asyncio
+async def test_get_fund_names(mcp_client: Client[FastMCPTransport]):
+    tool_result = await mcp_client.call_tool(
+        name="get_fund_names",
+        arguments={
+            "prn": "185045",
+        },
+    )
+    assert tool_result is not None
+    assert "ABERDEEN INVESTMENT FUNDS ICVC" in str(tool_result.data)
+
+
+@pytest.mark.asyncio
+async def test_get_fund_subfunds(mcp_client: Client[FastMCPTransport]):
+    tool_result = await mcp_client.call_tool(
+        name="get_fund_subfunds",
+        arguments={
+            "prn": "185045",
+        },
+    )
+    assert tool_result is not None
+    assert "abrdn China A Share Equity Fund" in str(tool_result.data)
