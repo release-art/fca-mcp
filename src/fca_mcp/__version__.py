d1e4a01098d854e732ec0d8021f339a464a82ae7
Ilja Orlovs <IljaOrlovs@users.noreply.github.com>
2026-04-21 14:23:30 +0100

    oh, the version should be a string after all

--- src/fca_mcp/__version__.py
+++ src/fca_mcp/__version__.py
@@ -4,8 +4,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-num_version = (0, 0, 0, "dev", 0)
 
-cache_version = ".".join(str(x) for x in num_version[:2])
-
-__version__ = ".".join(str(x) for x in num_version)
+__version__ = "0.0.0-dev0"
+cache_version = ".".join(__version__.split(".")[:2])  # "0.0"
